1bb336c107534dbf7ef78270e35f0b643234f062
phamhaiinamm <namphhe186407@fpt.edu.vn>
2026-02-03 21:14:53 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p> Thị Kitty là 1 con gà</p>
       </header>
     </div>
   );
